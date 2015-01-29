1c95319f88fb9bcb97aa58d10711d8c2d5e1a56e
Alexander V. Nikolaev <avn@daemon.hole.ru>
2015-01-29 21:24:31 +0200

    More filters

--- gmime/filter.go
+++ gmime/filter.go
@@ -96,3 +96,95 @@ func (f *aBestFilter) Encoding(constraint int) string {
 	e := C.g_mime_filter_best_encoding((*C.GMimeFilterBest)(f.pointer()), C.GMimeEncodingConstraint(constraint))
 	return goGMimeEncoding2String(e)
 }
+
+func NewCharsetFilter(source string, target string) *aFilter {
+	cSrc := C.CString(source)
+	defer C.free(unsafe.Pointer(cSrc))
+	cTgt := C.CString(target)
+	defer C.free(unsafe.Pointer(cTgt))
+	f := C.g_mime_filter_charset_new(cSrc, cTgt)
+	return castFilter(f)
+}
+
+func NewCRLFFilter(encode bool, dots bool) *aFilter {
+	f := C.g_mime_filter_crlf_new(gbool(encode), gbool(dots))
+	return castFilter(f)
+}
+
+const (
+	GMIME_FILTER_ENRICHED_IS_RICHTEXT int32 = (1 << 0)
+)
+
+func NewEnrichedFilter(flags int32) *aFilter {
+	f := C.g_mime_filter_enriched_new(C.guint32(flags))
+	return castFilter(f)
+}
+
+const (
+	GMIME_FILTER_FROM_MODE_DEFAULT = 0
+	GMIME_FILTER_FROM_MODE_ESCAPE  = 0
+	GMIME_FILTER_FROM_MODE_ARMOR   = 0
+)
+
+func MewFromFilter(flags int) *aFilter {
+	f := C.g_mime_filter_from_new(C.GMimeFilterFromMode(flags))
+	return castFilter(f)
+}
+
+const (
+	GMIME_FILTER_GZIP_MODE_ZIP   = C.GMIME_FILTER_GZIP_MODE_ZIP
+	GMIME_FILTER_GZIP_MODE_UNZIP = C.GMIME_FILTER_GZIP_MODE_UNZIP
+)
+
+func NewGZipFilter(mode int, level int) *aFilter {
+	f := C.g_mime_filter_gzip_new(C.GMimeFilterGZipMode(mode), C.int(level))
+	return castFilter(f)
+}
+
+const (
+	GMIME_FILTER_HTML_PRE               uint32 = (1 << 0)
+	GMIME_FILTER_HTML_CONVERT_NL               = (1 << 1)
+	GMIME_FILTER_HTML_CONVERT_SPACES           = (1 << 2)
+	GMIME_FILTER_HTML_CONVERT_URLS             = (1 << 3)
+	GMIME_FILTER_HTML_MARK_CITATION            = (1 << 4)
+	GMIME_FILTER_HTML_CONVERT_ADDRESSES        = (1 << 5)
+	GMIME_FILTER_HTML_ESCAPE_8BIT              = (1 << 6)
+	GMIME_FILTER_HTML_CITE                     = (1 << 7)
+)
+
+func NewHTMLFilter(flags uint32, colour int32) *aFilter {
+	f := C.g_mime_filter_html_new(C.guint32(flags), C.guint32(colour))
+	return castFilter(f)
+}
+
+func NewStripFilter() *aFilter {
+	f := C.g_mime_filter_strip_new()
+	return castFilter(f)
+}
+
+type MD5Filter interface {
+	Filter
+	GetDigest() []byte
+}
+
+type aFilterMD5 struct {
+	*aFilter
+}
+
+func NewMD5Filter() *aFilterMD5 {
+	f := castFilter(C.g_mime_filter_md5_new())
+	return &aFilterMD5{f}
+}
+
+func (m *aFilterMD5) GetDigest() []byte {
+	b := make([]byte, 16)
+	bp := &b[0]
+	mp := (*C.GMimeFilterMd5)(unsafe.Pointer(m.pointer()))
+	C.g_mime_filter_md5_get_digest(mp, (*C.uchar)(unsafe.Pointer(bp)))
+	return b
+}
+
+func NewYEncFilter(encode bool) *aFilter {
+	f := C.g_mime_filter_yenc_new(gbool(encode))
+	return castFilter(f)
+}
