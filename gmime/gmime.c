3e2dd39801cace26b54371f41972c178e28c0058
rhansen2 <rob.hansen@sendgrid.com>
2019-06-17 13:36:20 -0700

    Add function to set text without conversion

--- gmime/gmime.c
+++ gmime/gmime.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "gmime.h"
 
 
@@ -8,7 +9,7 @@ GMimeMessage *gmime_parse (const char *buffer, size_t len) {
 	GMimeMessage *message = g_mime_parser_construct_message (parser, NULL);
 	g_object_unref (parser);
 	if (!message) {
-		return NULL; 
+		return NULL;
 	}
 
 	return message;
@@ -54,4 +55,54 @@ GByteArray *gmime_get_bytes (GMimeObject *object) {
 
 	g_object_unref (stream);
 	return buf;
+}
+
+/**
+ * gmime_text_part_set_text:
+ * @mime_part: a #GMimeTextPart
+ * @text: the text in utf-8
+ *
+ * Sets the specified text as the content and updates the charset parameter on the Content-Type header.
+ **/
+void
+gmime_text_part_set_text (GMimeTextPart *mime_part, const char *text)
+{
+	GMimeContentType *content_type;
+	GMimeStream *filtered, *stream;
+	GMimeContentEncoding encoding;
+	GMimeDataWrapper *content;
+	GMimeFilter *filter;
+	const char *charset;
+	GMimeCharset mask;
+	size_t len;
+
+	g_return_if_fail (GMIME_IS_TEXT_PART (mime_part));
+	g_return_if_fail (text != NULL);
+
+	len = strlen (text);
+
+
+  charset = "utf-8";
+
+	content_type = g_mime_object_get_content_type ((GMimeObject *) mime_part);
+	g_mime_content_type_set_parameter (content_type, "charset", charset);
+
+	stream = g_mime_stream_mem_new_with_buffer (text, len);
+
+
+	content = g_mime_data_wrapper_new_with_stream (stream, GMIME_CONTENT_ENCODING_DEFAULT);
+	g_object_unref (stream);
+
+	g_mime_part_set_content ((GMimePart *) mime_part, content);
+	g_object_unref (content);
+
+	encoding = g_mime_part_get_content_encoding ((GMimePart *) mime_part);
+
+	/* if the user has already specified encoding the content with base64/qp/uu, don't change it */
+	if (encoding > GMIME_CONTENT_ENCODING_BINARY)
+		return;
+
+
+  g_mime_part_set_content_encoding ((GMimePart *) mime_part, GMIME_CONTENT_ENCODING_8BIT);
+
 }
--- gmime/gmime.h
+++ gmime/gmime.h
@@ -9,3 +9,4 @@ gboolean gmime_is_multi_part (GMimeObject *object);
 gboolean gmime_is_text_part (GMimeObject *object);
 void gmime_type_name(GMimeObject *object);
 GByteArray *gmime_get_bytes (GMimeObject *object);
+void gmime_text_part_set_text (GMimeTextPart *mime_part, const char *text);
